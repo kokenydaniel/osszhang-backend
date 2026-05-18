bddd7688617d7b8288b173efee79db7c33c83677
Daniel Kokeny <129154129+kokenydaniel@users.noreply.github.com>
2026-05-18 19:46:48 +0200

    fix: add missing register route to api.php

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
